fa4eb691d076280526b2756d27904c2d95fcca51
gaku.1237@icloud.com <ec2-user@ip-172-31-31-69.ec2.internal>
2021-01-01 04:05:47 +0000

    [Update]controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,20 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to blogs_path
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
