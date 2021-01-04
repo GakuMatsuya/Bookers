5cd8d4b441962af9c8d0e911a2d4bfe5d1a7a076
gaku.1237@icloud.com <ec2-user@ip-172-31-31-69.ec2.internal>
2021-01-04 09:34:37 +0000

    [Fix] create-action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,31 +4,40 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-  
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
     @book = Book.find(params[:id])
-    @books = Book.all
   end
   
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(book)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    book.destroy
+    redirect_to books_path
   end
   
   private
